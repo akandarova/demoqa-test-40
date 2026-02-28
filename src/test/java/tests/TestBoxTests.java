5f1f9fc324d9778151b2a8cd053d12c8be9e0e2e
alina1303 <7759682@gmail.com>
2026-02-28 11:04:00 +0300

    added new variables

--- src/test/java/tests/TestBoxTests.java
+++ src/test/java/tests/TestBoxTests.java
@@ -18,7 +18,7 @@ public class TestBoxTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.pageLoadTimeout = 50_000;
+        Configuration.pageLoadTimeout = 70_000;
     }
 
     @AfterEach
@@ -38,27 +38,21 @@ public class TestBoxTests {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click();
-        $(byText("June")).click();
-        $(".react-datepicker__year-select").click();
-        $(byText("2000")).click();
-        $(".react-datepicker__month").$(byText("14")).click();
-        $("#subjectsInput").setValue("o");
-        $(byText("Commerce")).click();
-        $("#hobbies-checkbox-2").click();
-        $("#uploadPicture").uploadFromClasspath("file.png");
+        $(".react-datepicker__month-select").$(byText(monthOfBirth)).click();
+        $(".react-datepicker__year-select").$(byText(yearOfBirth)).click();
+        $(".react-datepicker__day--0" + dayOfBirth +":not(.react-datepicker__month)").click();
+        $("#subjectsInput").setValue(userSubject).pressEnter();
+        $("#hobbiesWrapper").$(byText(userHobbie)).click();
+        $("#uploadPicture").uploadFile(userFile);
         $("#currentAddress").setValue(currentAddress);
-        $("#state").scrollTo().click();
-        $("#react-select-3-input").click();
-        $(byText("Uttar Pradesh")).click();
-        $("#react-select-4-input").click();
-        $(byText("Lucknow")).click();
+        $("#state").click();
+        $("#react-select-3-input").setValue(userState).pressEnter();
+        $("#city").click();
+        $("#react-select-4-input").setValue(userCity).pressEnter(); //переменную
         $("#submit").click();
-
-        //Selenide.executeJavaScript("document.querySelector('.ad-container').remove();"); он был нужен, когда пыталась решить проблему с недоступностью элемента
 
         $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
         $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(userEmail));
@@ -66,11 +60,10 @@ public class TestBoxTests {
         $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(userNumber));
         $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("14 June,2000"));
         $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Commerce"));
-        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("file.png"));
         $(".table-responsive").$(byText("Address")).parent().shouldHave(text(currentAddress));
-        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Lucknow"));
-
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text(userState + " " + userCity));
     }
 
     @Test
@@ -84,7 +77,7 @@ public class TestBoxTests {
         $$(".router-link").findBy(text("Practice Form")).click();
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue(userNumber);
         $("#submit").scrollTo().click();
 
@@ -103,13 +96,12 @@ public class TestBoxTests {
                 """);
         $$(".card-body").findBy(text("Forms")).click();
         $$(".router-link").findBy(text("Practice Form")).click();
-        $("#firstName").setValue("Alex");
-        $("#userNumber").setValue("8900562323");
+        $("#firstName").setValue(firstName);
+        $("#userNumber").setValue(userNumber);
         $("#submit").scrollTo().click();
 
         $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         $("#genterWrapper").$(byText("Male")).shouldHave(cssValue("color", "rgba(220, 53, 69, 1)"));
-
     }
 
     @Test
@@ -124,12 +116,11 @@ public class TestBoxTests {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail+"889");
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue(userNumber);
         $("#submit").scrollTo().click();
 
         $("#userEmail").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
-
     }
 
     @Test
@@ -143,12 +134,11 @@ public class TestBoxTests {
         $$(".router-link").findBy(text("Practice Form")).click();
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("8900");
         $("#submit").scrollTo().click();
 
         $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
-
     }
 }
 
--- src/test/java/tests/testData/TestData.java
+++ src/test/java/tests/testData/TestData.java
@@ -1,9 +1,19 @@
 package tests.testData;
 
+import java.io.File;
+
 public class TestData {
     public static String firstName = "Alex";
     public static String lastName = "Ivanov";
     public static String userEmail = "AlexIvanov@mail.ru";
     public static String userNumber = "8900562323";
+    public static String monthOfBirth = "June";
+    public static String yearOfBirth = "2000";
+    public static String dayOfBirth = "14";
+    public static String userSubject = "Commerce";
+    public static String userHobbie = "Music";
+    public static File userFile = new File("src/test/resources/file.png");
     public static String currentAddress = "First street 1";
+    public static String userState = "Uttar Pradesh";
+    public static String userCity = "Lucknow";
 }
